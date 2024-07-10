7b692e89bf02679964ebbdbef2e0fd48db5a0946
Anwar Louis <smmaniac@hotmail.co.uk>
2024-07-10 15:27:58 +0100

    Fix aggregate indexing bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,56 @@ import Distance from './components/Distance';
 function App() {
   const fitnessData = fitData.map(function (entry) {
     if (entry.fitnessActivity === 'biking') {
-      let miles = entry.aggregate[2].floatValue * 0.000621371;
-      let mph = entry.aggregate[3].floatValue * 2.23694;
-      let kmh = entry.aggregate[3].floatValue * 3.6;
+      let distanceIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.distance.delta';
+      });
+
+      let speedIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.speed.summary';
+      });
+
+      let durationIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.active_minutes';
+      });
+
+      let caloriesIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.calories.expended';
+      });
+
+      let miles = distanceIndex.floatValue * 0.000621371;
+      let mph = speedIndex.floatValue * 2.23694;
+      let kmh = speedIndex.floatValue * 3.6;
 
       return {
         activity: entry.fitnessActivity,
-        duration: entry.aggregate[4].floatValue,
-        calories: entry.aggregate[1].floatValue,
+        duration: durationIndex.floatValue,
+        calories: Math.round(caloriesIndex.floatValue * 100) / 100,
         miles: Math.round(miles * 100) / 100,
-        kilometers: Math.round(entry.aggregate[2].floatValue / 10) / 100,
+        kilometers: Math.round(distanceIndex.floatValue / 10) / 100,
         averageMPH: Math.round(mph * 100) / 100,
         averageKMH: Math.round(kmh * 100) / 100,
         date: entry.startTime,
       };
     } else if (entry.fitnessActivity === 'walking') {
-      let milesWalked = entry.aggregate[3].floatValue * 0.000621371;
-      let kmWalked = entry.aggregate[3].floatValue * 0.001;
+      let distanceIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.distance.delta';
+      });
+
+      let stepsIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.step_count.delta';
+      });
+
+      let durationIndex = entry.aggregate.find(function (ind) {
+        return ind.metricName === 'com.google.active_minutes';
+      });
+
+      let milesWalked = distanceIndex.floatValue * 0.000621371;
+      let kmWalked = distanceIndex.floatValue * 0.001;
 
       return {
         activity: entry.fitnessActivity,
-        steps: entry.aggregate[2].intValue,
-        duration: entry.aggregate[5].intValue,
+        steps: stepsIndex.intValue,
+        duration: durationIndex.intValue,
         kilometers: Math.round(kmWalked * 100) / 100,
         miles: Math.round(milesWalked * 100) / 100,
       };
