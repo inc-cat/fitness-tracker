b5e1e4b0848eafded93d2bd0026f79fed5bb5cb5
Anwar Louis <smmaniac@hotmail.co.uk>
2024-07-24 19:41:25 +0100

    Enforce undefined entry read skip

--- src/App.jsx
+++ src/App.jsx
@@ -5,64 +5,72 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Distance from './components/Distance';
 
 function App() {
-  const fitnessData = fitData.map(function (entry) {
-    if (entry.fitnessActivity === 'biking') {
-      let distanceIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.distance.delta';
-      });
+  const fitnessData = fitData
+    .map(function (entry) {
+      if (entry.fitnessActivity === 'biking') {
+        let distanceIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.distance.delta';
+        });
 
-      let speedIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.speed.summary';
-      });
+        let speedIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.speed.summary';
+        });
 
-      let durationIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.active_minutes';
-      });
+        let durationIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.active_minutes';
+        });
 
-      let caloriesIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.calories.expended';
-      });
+        let caloriesIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.calories.expended';
+        });
 
-      let miles = distanceIndex.floatValue * 0.000621371;
-      let mph = speedIndex.floatValue * 2.23694;
-      let kmh = speedIndex.floatValue * 3.6;
+        if (!distanceIndex) {
+          return;
+        }
 
-      return {
-        activity: entry.fitnessActivity,
-        duration: durationIndex.intValue,
-        calories: caloriesIndex.floatValue,
-        miles: miles,
-        kilometers: distanceIndex.floatValue / 1e3,
-        averageMPH: mph,
-        averageKMH: kmh,
-        date: entry.startTime,
-      };
-    } else if (entry.fitnessActivity === 'walking') {
-      let distanceIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.distance.delta';
-      });
+        let miles = distanceIndex.floatValue * 0.000621371;
+        let mph = speedIndex.floatValue * 2.23694;
+        let kmh = speedIndex.floatValue * 3.6;
 
-      let stepsIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.step_count.delta';
-      });
+        return {
+          activity: entry.fitnessActivity,
+          duration: durationIndex.intValue,
+          calories: caloriesIndex.floatValue,
+          miles: miles,
+          kilometers: distanceIndex.floatValue / 1e3,
+          averageMPH: mph,
+          averageKMH: kmh,
+          date: entry.startTime,
+        };
+      } else if (entry.fitnessActivity === 'walking') {
+        let distanceIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.distance.delta';
+        });
 
-      let durationIndex = entry.aggregate.find(function (ind) {
-        return ind.metricName === 'com.google.active_minutes';
-      });
+        let stepsIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.step_count.delta';
+        });
 
-      let milesWalked = distanceIndex.floatValue * 0.000621371;
-      let kmWalked = distanceIndex.floatValue * 0.001;
+        let durationIndex = entry.aggregate.find(function (ind) {
+          return ind.metricName === 'com.google.active_minutes';
+        });
 
-      return {
-        activity: entry.fitnessActivity,
-        steps: stepsIndex.intValue,
-        duration: durationIndex.intValue,
-        kilometers: Math.round(kmWalked * 100) / 100,
-        miles: Math.round(milesWalked * 100) / 100,
-        date: entry.startTime,
-      };
-    }
-  });
+        let milesWalked = distanceIndex.floatValue * 0.000621371;
+        let kmWalked = distanceIndex.floatValue * 0.001;
+
+        return {
+          activity: entry.fitnessActivity,
+          steps: stepsIndex.intValue,
+          duration: durationIndex.intValue,
+          kilometers: Math.round(kmWalked * 100) / 100,
+          miles: Math.round(milesWalked * 100) / 100,
+          date: entry.startTime,
+        };
+      }
+    })
+    .filter(function (entry) {
+      return entry !== undefined;
+    });
 
   return (
     <>
